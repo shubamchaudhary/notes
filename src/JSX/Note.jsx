9c455d60981a9d34615a4901772b5f03150aa3d7
SHUBAM CHAUDHARY <110082517+shubamchaudhary@users.noreply.github.com>
2023-07-06 02:58:50 +0530

    Update Note.jsx

--- src/JSX/Note.jsx
+++ src/JSX/Note.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import DeleteIcon from "@material-ui/icons/Delete";
 import Draggable from "react-draggable";
+import {BiNotepad} from "react-icons/bi"
 
 function Note(props) {
   function handleClick() {
@@ -11,7 +12,7 @@ function Note(props) {
     <div className=" hover:h-10 ">
     <Draggable >
     <div className="note text-center shadow-md hover:shadow-lg hover:font-semibold hover:text-3xl">
-      <h1 className="font-bold hover:text-5xl">{props.title}</h1>
+     <h1 className="font-bold hover:cursive hover:uppercase hover:text-[#ffab4a] hover:text-4xl"><BiNotepad className=" absolute   text-[#ffab4a]"/>{props.title}</h1>
       <h4 className="hover:text:4xl">{props.content}</h4>
       <button onClick={handleClick}>
         <DeleteIcon />
