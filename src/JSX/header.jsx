4daef000bf937db3ad41e7e7d260788b7726d580
SHUBAM CHAUDHARY <110082517+shubamchaudhary@users.noreply.github.com>
2023-07-31 22:30:26 +0530

    Update header.jsx

--- src/JSX/header.jsx
+++ src/JSX/header.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import {BiNotepad} from "react-icons/bi"
 import { useState,useEffect } from "react";
-import {BsLightningChargeFill} from 'react-icons/bs'
+import {ImBrightnessContrast} from 'react-icons/im'
 
 function Header(){
     const [darkMode, setDarkMode] = useState( JSON.parse(localStorage.getItem("darkMode")) || false);
@@ -18,7 +18,7 @@ function Header(){
      return <div className="">
         <header className="shadow-lg flex justify-between w-full  shadow-gray-400 h-[110px] text-4xl dark:bg-darkbg bg-[#f6b607cc] ">
        <h1 className='text-7xl pl-12 pt-5 pb-5 text-white cursive flex'> <BiNotepad className="mx-6 "/> NOTES </h1>
-       <button className="mr-16 text-white text-[50px] dark:text-gray-500" onClick={() => setDarkMode(!darkMode)} ><BsLightningChargeFill/></button>
+       <button className="mr-16 text-darkbg text-[50px] dark:text-[#f6b607cc]" onClick={() => setDarkMode(!darkMode)} ><ImBrightnessContrast/></button>
     </header>
       </div>
 }
