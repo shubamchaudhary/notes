27ade6ea87abae2a48b53306129fa274e507b380
SHUBAM CHAUDHARY <110082517+shubamchaudhary@users.noreply.github.com>
2023-07-31 20:19:59 +0530

    Update Note.jsx

--- src/JSX/Note.jsx
+++ src/JSX/Note.jsx
@@ -14,7 +14,7 @@ function Note(props) {
     <Draggable >
     <div className="note  dark:bg-gray-700 cursor-pointer text-center shadow-md  hover:shadow-lg hover:font-semibold hover:text-3xl">
       <h1 className="font-bold dark:text-gray-300 hover:cursive hover:uppercase hover:text-[#ffab4a] hover:text-4xl"><BiNotepad className=" absolute   text-[#ffab4a]"/>{props.title}</h1>
-      <h4 className=" dark:text-gray-300  hover:text:4xl">{props.content}</h4>
+      <h4 className=" dark:text-gray-300  hover:text:2xl">{props.content}</h4>
       <button onClick={handleClick}>
         <DeleteIcon />
       </button>
