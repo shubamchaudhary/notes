d1cc5b33e5e54315414517b9dfa0a4d07d35f529
SHUBAM CHAUDHARY <110082517+shubamchaudhary@users.noreply.github.com>
2023-07-31 18:52:47 +0530

    Update header.jsx

--- src/JSX/header.jsx
+++ src/JSX/header.jsx
@@ -1,10 +1,26 @@
 import React from "react";
 import {BiNotepad} from "react-icons/bi"
+import { useState,useEffect } from "react";
+import {BsLightningChargeFill} from 'react-icons/bs'
 
 function Header(){
-   return <header className=" h-auto text-4xl  bg-[#ffab4a] ">
+    const [darkMode, setDarkMode] = useState( JSON.parse(localStorage.getItem("darkMode")) || false);
+    useEffect(() => {
+        localStorage.setItem("darkMode", JSON.stringify(darkMode));
+    
+        if(darkMode){
+          document.documentElement.classList.add('dark');
+        } else {
+          document.documentElement.classList.remove('dark');
+        }
+        
+      }, [darkMode]);
+     return <div className="">
+        <header className="shadow-lg flex justify-between w-full  shadow-gray-400 h-[110px] text-4xl dark:bg-darkbg bg-[#f6b607cc] ">
        <h1 className='text-7xl pl-12 pt-5 pb-5 text-white cursive flex'> <BiNotepad className="mx-6 "/> NOTES </h1>
+       <button className="mr-16 text-white text-[50px] dark:text-gray-500" onClick={() => setDarkMode(!darkMode)} ><BsLightningChargeFill/></button>
     </header>
+      </div>
 }
 
 export default Header
